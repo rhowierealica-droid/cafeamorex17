20b77b579f7a15cd5a8f4710d0a9889afbffadda
rhowierealica-droid <rhowierealica@gmail.com>
2025-10-15 05:05:29 +0800

    Add files via upload

---
+++ public/AccessCustomer.js
@@ -0,0 +1,16 @@
+import { auth, db } from './firebase-config.js';
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const userDoc = await getDoc(doc(db, "users", user.uid));
+  if (!userDoc.exists() || userDoc.data().role !== "Customer") {
+    alert("Access denied. Customers only.");
+    window.location.href = "login.html";
+  }
+});
